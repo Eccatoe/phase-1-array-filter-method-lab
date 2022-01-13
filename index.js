5231c3e63e4466440ccd34e78f3c0d92359591f4
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-12 21:36:13 -0500

    commit tree 36b164096dee7ebc6f482f101f62432d492f65cc

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+const findMatching=(driverArray, enteredName)=>{
+    return (driverArray.filter(driverName=>driverName.toLowerCase()===enteredName.toLowerCase()))
+}
+
+const fuzzyMatch=(driverArray, enteredLetters)=>{
+    let endNum = enteredLetters.length
+
+    return (driverArray.filter(driverName=> {
+        return driverName.slice(0, endNum).toLowerCase() === enteredLetters.toLowerCase()
+    }))
+}
+
+// const matchName=(driverArray, enteredName)=>{
+//     let x=driverArray.indexOf(enteredName)
+//     let obj=driverArray[x]
+//     return (driverArray.filter(obj=>obj===driverArray.indexOf(enteredName)))
+//     // driverArray.indexOf(enteredName)
+//     }   
+
+// // matchName(drivers, "Bobby")
+// // return driveryArray[x] that is at the //name
+// // x=indexOf(enteredName)
+
+const matchName=(driverArray, string)=>{
+    let match=driverArray.filter((obj)=>obj.name===string)
+    if (match) {return match}
+}
